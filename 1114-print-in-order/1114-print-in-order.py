bb7e94f683dc1a040a8d7e7b7c67122f95389ebc
Enam Mijbah Noor <enammijbahnoor@gmail.com>
2022-04-20 01:13:38 +0600

    Time: 73 ms (61.39%), Space: 14.5 MB (62.05%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.py
+++ 1114-print-in-order/1114-print-in-order.py
@@ -1,30 +1,20 @@
+from threading import Event
+
+
 class Foo:
     def __init__(self):
-        self.first_finished = False
-        self.second_finished = False
-
+        self.first_done = Event()
+        self.second_done = Event()
 
     def first(self, printFirst: 'Callable[[], None]') -> None:
-        
-        # printFirst() outputs "first". Do not change or remove this line.
         printFirst()
-        
-        self.first_finished = True
-
+        self.first_done.set()
 
     def second(self, printSecond: 'Callable[[], None]') -> None:
-        while not self.first_finished:
-            pass
-        
-        # printSecond() outputs "second". Do not change or remove this line.
+        self.first_done.wait()
         printSecond()
-        
-        self.second_finished = True
-
+        self.second_done.set()
 
     def third(self, printThird: 'Callable[[], None]') -> None:
-        while not self.second_finished:
-            pass
-        
-        # printThird() outputs "third". Do not change or remove this line.
+        self.second_done.wait()
         printThird()
