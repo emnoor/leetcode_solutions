8034335bf0b83b824e35c44a537828767790d0c0
Enam Mijbah Noor <enammijbahnoor@gmail.com>
2022-04-20 21:34:22 +0600

    Time: 152 ms (5.72%), Space: 15.1 MB (15.00%) - LeetHub

---
+++ 1116-print-zero-even-odd/1116-print-zero-even-odd.py
@@ -0,0 +1,28 @@
+from queue import Queue
+
+
+class ZeroEvenOdd:
+    def __init__(self, n):
+        self.n = n
+        self.zero_q = Queue()
+        self.zero_q.put(1)
+        self.other_qs = [Queue(), Queue()]
+
+	# printNumber(x) outputs "x", where x is an integer.
+    def zero(self, printNumber: 'Callable[[int], None]') -> None:
+        for i in range(1, self.n+1):
+            self.zero_q.get()
+            printNumber(0)
+            self.other_qs[i % 2].put(1)
+        
+    def even(self, printNumber: 'Callable[[int], None]') -> None:
+        for i in range(2, self.n+1, 2):
+            self.other_qs[0].get()
+            printNumber(i)
+            self.zero_q.put(1)
+        
+    def odd(self, printNumber: 'Callable[[int], None]') -> None:
+        for i in range(1, self.n+1, 2):
+            self.other_qs[1].get()
+            printNumber(i)
+            self.zero_q.put(1)
