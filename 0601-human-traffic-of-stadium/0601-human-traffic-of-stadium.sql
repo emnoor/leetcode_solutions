e97b9d598ff842ff6224371bf1bbd3a0b6b1464c
Enam Mijbah Noor <enammijbahnoor@gmail.com>
2023-07-21 21:35:30 +0600

    Time: 813 ms (45.28%), Space: 0B (100.00%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -1,14 +1,18 @@
-WITH allids AS (SELECT id FROM Stadium WHERE people >= 100)
-SELECT id, visit_date, people
+WITH validids AS (SELECT id
+                  FROM Stadium
+                  WHERE people >= 100)
+SELECT id
+     , visit_date
+     , people
 FROM Stadium
-WHERE ((id + 0) IN (SELECT id FROM allids)
-        AND (id + 1) IN (SELECT id FROM allids)
-        AND (id + 2) IN (SELECT id FROM allids))
-   OR ((id - 1) IN (SELECT id FROM allids)
-        AND (id + 0) IN (SELECT id FROM allids)
-        AND (id + 1) IN (SELECT id FROM allids))
-   OR ((id - 2) IN (SELECT id FROM allids)
-        AND (id - 1) IN (SELECT id FROM allids)
-        AND (id + 0) IN (SELECT id FROM allids))
+WHERE (id IN (SELECT id FROM validids)
+        AND (id + 1) IN (SELECT id FROM validids)
+        AND (id + 2) IN (SELECT id FROM validids))
+   OR ((id - 1) IN (SELECT id FROM validids)
+        AND id IN (SELECT id FROM validids)
+        AND (id + 1) IN (SELECT id FROM validids))
+   OR ((id - 2) IN (SELECT id FROM validids)
+        AND (id - 1) IN (SELECT id FROM validids)
+        AND id IN (SELECT id FROM validids))
 ORDER BY visit_date
 ;
