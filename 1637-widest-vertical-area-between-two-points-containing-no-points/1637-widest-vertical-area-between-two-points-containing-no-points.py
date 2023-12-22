6ab4d60fce342da85999f10edefd92fcdcdcaa85
Enam Mijbah Noor <enammijbahnoor@gmail.com>
2023-12-22 20:02:26 +0600

    Time: 715 ms (97.92%), Space: 59.2 MB (77.92%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.py
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.py
@@ -4,7 +4,7 @@ class Solution:
         
         d = 0
         for i in range(len(points) - 1):
-            nd = points[i][0] - points[i-1][0]
+            nd = points[i+1][0] - points[i][0]
             if nd > d:
                 d = nd
         return d
