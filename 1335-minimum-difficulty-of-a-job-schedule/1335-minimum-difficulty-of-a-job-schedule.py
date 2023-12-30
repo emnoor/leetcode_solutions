889318083835a8bc801e8e150fba6527f7f9ec7e
Enam Mijbah Noor <enammijbahnoor@gmail.com>
2023-12-30 20:39:58 +0600

    Time: 721 ms (37.95%), Space: 18.8 MB (5.46%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -0,0 +1,28 @@
+from functools import cache
+
+
+class Solution:
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        n = len(jobDifficulty)
+
+        @lru_cache(None)
+        def solve(start: int, d: int) -> int:
+            if n - start < d:
+                return -1
+
+            if d == 1:
+                return max(jobDifficulty[start:])
+
+            min_total_diff = float('inf')
+            max_diff = -1
+            for i in range(start, n):
+                if jobDifficulty[i] > max_diff:
+                    max_diff = jobDifficulty[i]
+
+                r = solve(i + 1, d - 1)
+                if r >= 0 and max_diff + r < min_total_diff:
+                    min_total_diff = max_diff + r
+
+            return min_total_diff
+
+        return solve(0, d)
